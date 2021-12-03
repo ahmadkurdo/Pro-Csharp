e86b61f867cb088b446106c91abefcf2d2ef7150
Ahmed <ahmad.rizgar.rashid@gmail.com>
2021-12-03 19:51:54 +0100

    Tell other thread we are done by using handle set

--- Threading/AutoResetEventExample.cs
+++ Threading/AutoResetEventExample.cs
@@ -5,9 +5,9 @@ namespace SmartThreading
 {
     public class AutoResetEventExample
     {
+        static AutoResetEvent _waitHandle = new AutoResetEvent(false);
         public static void Run()
         {
-            AutoResetEvent _waitHandle = new AutoResetEvent(false);
             Console.WriteLine("***** Adding with Thread objects *****");
             Console.WriteLine("ID of thread in Main(): {0}",Thread.CurrentThread.ManagedThreadId);
             
@@ -27,6 +27,8 @@ namespace SmartThreading
             {
                 Console.WriteLine("ID of thread in Add(): {0}",Thread.CurrentThread.ManagedThreadId);
                 Console.WriteLine("{0} + {1} is {2}",ap.a, ap.b, ap.a + ap.b);
+                // Tell other thread we are done.
+                _waitHandle.Set();
             }
         }
         
